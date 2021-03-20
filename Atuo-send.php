3498797eb27b28df560dd461dc11ed96695a8dd2
Alireza <74488703+Alireza-ME@users.noreply.github.com>
2021-03-20 11:14:27 +0330

    update Atuo-send.php

--- Atuo-send.php
+++ Atuo-send.php
@@ -1,1 +1,27 @@
 <?php
+
+include 'config.php';
+function bot($method,$datas=[]){
+    $url = 'https://api.telegram.org/bot'.API_KEY.'/'.$method;
+$ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+$token= API_KEY; $channel= Channel; $send = Text;
+
+$ids = ['ProxyMTProto','TelMTProto','MyPoroxy','JackalProxy','Cheetah_MTP'];
+$id = $ids[array_rand($ids)];
+$proxy = json_decode(file_get_contents("https://poty.fun/apis/proxy.php?channel=$id"), true)['results'];
+$prx = $proxy[0]['link'];
+
+
+bot('sendMessage',['chat_id'=>$channel,'text'=>"$send\n$prx"]);
+
